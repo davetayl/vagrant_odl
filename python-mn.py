65925d1091286a775faaabaf1f9d6608c37fec68
David Taylor <dave@imagetaylor.com>
2020-03-08 16:11:50 +1100

    cleaned up mininet python script

--- python-mn.py
+++ python-mn.py
@@ -7,6 +7,11 @@ from mininet.topolib import TreeTopo
 from mininet.cli import CLI
 from mininet.node import Controller, OVSKernelSwitch, RemoteController
 
+# Configure constants
+ctrIp = "10.0.0.17"
+ctrPort = 6633
+ctrName = "odl01"
+
 # Make sure there is only one argument passed
 if len(sys.argv) == 2:
     topoSel = sys.argv[1]
@@ -23,7 +28,7 @@ topoDict = {
 # Construct net objects
 myTopo = TreeTopo(depth=topoDict[topoSel][0],fanout=topoDict[topoSel][1])
 myNet = Mininet(controller=RemoteController, switch=OVSKernelSwitch, topo=myTopo)
-odl = myNet.addController('odl', controller=RemoteController, ip="10.0.0.17", port=6633)
+myNet.addController(ctrName, controller=RemoteController, ip=ctrIp, port=ctrPort)
 
 # Start the mininet
 myNet.start()
