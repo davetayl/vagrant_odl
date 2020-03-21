088132ef8cc9a9f1217972ae705ab9399d675c5f
David Taylor <dave@imagetaylor.com>
2020-03-22 03:14:23 +1100

    Added ODL features

--- provision-odl.sh
+++ provision-odl.sh
@@ -40,9 +40,14 @@ echo "- OpenDaylight (Sodium) installed -"
 # Configure ODL
 /sbin/setenforce 0 > /dev/null 2>&1
 /bin/sed -i 's/odl/root/g' /usr/lib/systemd/system/opendaylight.service > /dev/null 2>&1
+/bin/sed -i 's/a126e2e0-3c77-4543-be57-883c02d3759e/a126e2e0-3c77-4543-be57-883c02d3759e,odl-controller-model-topology,odl-netconf-api,odl-openflowjava-protocol,odl-openflowplugin-app-arbitratorreconciliation,odl-openflowplugin-app-config-pusher,odl-openflowplugin-app-forwardingrules-manager,odl-openflowplugin-app-reconciliation-framework,odl-openflowplugin-app-topology,odl-openflowplugin-app-topology-manager,odl-openflowplugin-flow-services,odl-openflowplugin-flow-services-rest,odl-openflowplugin-libraries,odl-openflowplugin-nsf-model,odl-openflowplugin-southbound,odl-restconf/g' /opt/opendaylight/etc/org.apache.karaf.features.cfg > /dev/null 2>&1
 /bin/systemctl enable opendaylight > /dev/null 2>&1
 /bin/systemctl start opendaylight > /dev/null 2>&1
 echo "- OpenDaylight server configured -"
+echo ""
+echo "Log in with \"ssh -p 8101 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no karaf@localhost\""
+echo "Pasword: karaf"
+echo ""
 
 echo "-----------------------------------------"
 echo "With great power comes great opportunity!"
